69923825be23cacd2013950d68a62dba41ce2c67
tmdgns1507 <tmdgns1507@gmail.com>
2022-07-19 04:15:38 +0900

    [Week08] 8주차 풀이 Add
    https://leetcode.com/problems/search-insert-position/
    
    https://leetcode.com/problems/valid-sudoku/
    
    https://leetcode.com/problems/sudoku-solver/

---
+++ Seunghoon/Week08/SearchInsertPosition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmStudy08
+{
+
+// 2022-07-11 ~ 2022-07-16
+// Week08
+// Search Insert Position
+// https://leetcode.com/problems/search-insert-position/submissions/
+
+    class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            List<int> numList = nums.ToList();
+
+            if (!numList.Contains(target))
+            {
+                numList.Add(target);
+                numList.Sort();
+            }
+
+            for (int i = 0; i < numList.Count; i++)
+            {
+                if (numList[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Seunghoon/Week08/ValidSudoku.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmStudy08
+{
+
+// 2022-07-11 ~ 2022-07-16
+// Week08
+// Valid Sudoku
+// https://leetcode.com/problems/valid-sudoku/submissions/
+
+    class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            HashSet<char>[] boardSet = new HashSet<char>[27];
+
+            for (int i = 0; i < boardSet.Length; i++)
+            {
+                boardSet[i] = new HashSet<char>();
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] != '.' && !IsValid(board, i, j, boardSet)) return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsValid(char[][] board, int i, int j, HashSet<char>[] boardSet)
+        {
+            bool IsValid(int set)
+            {
+                if (boardSet[set].Contains(board[i][j])) return false;
+                boardSet[set].Add(board[i][j]);
+
+                return true;
+            }
+
+            return IsValid(i) && IsValid(9 + j) && IsValid(18 + i / 3 * 3 + j / 3);
+        }
+    }
+}
